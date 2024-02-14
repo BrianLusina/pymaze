35f6c643bfe25b5a9841f3006eb5ed2472f5bcf3
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-02-14 18:19:17 +0300

    feat(view): primitives - protocol & euclidean point
    Adds a Primitive protocol that acts as an interface/protocol for static duck
    typing that will be common for all primitives(lines, points, polyglons, rectanlges, etc).
    
    Additionally adds a Euclidean point that implements the draw method of the protocol
    to returna Python string of the point and also provide a translate method to return a new
    Euclidean point.
    This point subclasses the NamedTuple class to make it immutable.

--- src/pymaze/view/primitives.py
+++ src/pymaze/view/primitives.py
@@ -1,3 +1,38 @@
+"""
+Primitives that will be used to create XML tags for SVG graphics
+"""
+from typing import Protocol, NamedTuple
+
+
+class Primitive(Protocol):
+    """
+    Protocol or interface common to all primitives.
+    Reference: https://docs.python.org/3/library/typing.html#typing.Protocol
+    """
+
+    def draw(self, **attributes) -> str:
+        """method that is common on all primitives"""
+        ...
+
+
+class Point(NamedTuple):
+    """
+    Euclidean point comprising x and y coordinates. Note that named tuples are immutable and therefore the translate
+    method will return a new Euclidean point.
+    """
+
+    x: int
+    y: int
+
+    def draw(self, **attributes) -> str:
+        """draws and x and y point"""
+        return f"{self.x},{self.y}"
+
+    def translate(self, x=0, y=0) -> 'Point':
+        """Translates x & y points into a new Euclidean point."""
+        return Point(x=self.x + x, y=self.y + y)
+
+
 def tag(name: str, value: str | None = None, **attributes) -> str:
     """
     Generic function that returns an XML tag with the given name, optional value and zero or more attributes
